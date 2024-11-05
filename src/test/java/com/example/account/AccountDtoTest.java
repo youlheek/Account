a6bde23d0dcb2567839c88dce2c4c9a4297d3c22
youlheek <youlheek@gmail.com>
2024-11-05 22:12:19 +0900

    @Getter @Setter

---
+++ src/test/java/com/example/account/AccountDtoTest.java
@@ -0,0 +1,18 @@
+package com.example.account;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountDtoTest {
+
+    @Test
+    void accountDto() {
+        //given
+        //when
+        //then
+
+        AccountDto accountDto = new AccountDto();
+
+    }
+}
